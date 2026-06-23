bdb69f93f88502bb368d57674aec027b7924120c
Priya-Kolay <priyakolay10@gmail.com>
2026-06-23 12:01:04 +0530

    Added JUnit AAA Pattern Exercise

---
+++ Week1/TDD/AAAExercise/src/test/java/CalculatorAAATest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorAAATest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up test...");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up after test...");
+        calculator = null;
+    }
+
+    @Test
+    public void testAdd() {
+
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(5, result);
+    }
+}
