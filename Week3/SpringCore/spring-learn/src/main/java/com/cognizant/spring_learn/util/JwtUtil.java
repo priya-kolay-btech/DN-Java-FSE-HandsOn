e6ce1992618a0871f4ba0a3bd1637f8472319748
Priya-Kolay <priyakolay10@gmail.com>
2026-07-09 02:20:55 +0530

    Completed Week 3 JWT_Token generation

---
+++ Week3/SpringCore/spring-learn/src/main/java/com/cognizant/spring_learn/util/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.cognizant.spring_learn.util;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET =
+            "abcdefghijklmnopqrstuvwxyz123456";
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
