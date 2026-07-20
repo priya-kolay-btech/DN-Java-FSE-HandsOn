c265a11999b522d100cffaec9fae9a8731f67a75
Priya-Kolay <priyakolay10@gmail.com>
2026-07-21 02:11:02 +0530

    Added Eureka Discovery Server, API Gateway and Greet Service--Complted microservices

---
+++ Week4/api-gateway/src/main/java/com/cognizant/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
+
+// package com.cognizant.api.gateway;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+// @SpringBootApplication
+// @EnableDiscoveryClient
+// public class ApiGatewayApplication {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(ApiGatewayApplication.class, args);
+//     }
+
+// }
